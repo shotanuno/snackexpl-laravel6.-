eeb98e06be7aa1bad61bf867d1520627097d39a8
shotanuno <20ca049e@rikkyo.ac.jp>
2022-10-21 17:34:15 +0900

    add:
    ログイン機能の実装(仮)

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::put('/snacks/{snack}', 'SnackController@update');
 Route::post('/snacks', 'SnackController@store');
 Route::delete('/snacks/{snack}', 'SnackController@delete');
 
+Auth::routes();
+
 ?>
