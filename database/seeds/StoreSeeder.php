c55348f1c41a387ca151b9891f1db65f02f42ce5
shotanuno <20ca049e@rikkyo.ac.jp>
2022-10-21 12:32:58 +0900

    fix:
    ストアのシーダーファイルの編集

--- database/seeds/StoreSeeder.php
+++ database/seeds/StoreSeeder.php
@@ -13,7 +13,6 @@ class StoreSeeder extends Seeder
     {
     
             DB::table('stores')->insert([
-                'id' => 1,
                 'name' => 'セブンイレブン',
                 'overview' => '圧倒的高品質。値段が他店より高い,若しくは量が少ない分とても高品質な商品が多い。',
                 'created_at' => new DateTime(),
@@ -23,7 +22,6 @@ class StoreSeeder extends Seeder
             ]);
     
             DB::table('stores')->insert([
-                'id' => 2,
                 'name' => 'ファミリーマート',
                 'overview' => 'とても無難。セブンイレブンに比べ質が少し落ちる分,量がある商品が多い。',
                 'created_at' => new DateTime(),
@@ -33,7 +31,6 @@ class StoreSeeder extends Seeder
             ]);
     
             DB::table('stores')->insert([
-                'id' => 3,
                 'name' => 'ローソン',
                 'overview' => 'コンビニ菓子の中では良くも悪くも目立たない。',
                 'created_at' => new DateTime(),
@@ -42,9 +39,8 @@ class StoreSeeder extends Seeder
             ]);
     
             DB::table('stores')->insert([
-                'id' => 4,
                 'name' => 'まいばすけっと',
-                'overview' => '四種の店の中で唯一コンビニではない枠。この店の一番の強みは何といっても安',
+                'overview' => '四種の店の中で唯一コンビニではない枠。この店の一番の強みは何といっても安さ',
                 'created_at' => new DateTime(),
                 'updated_at' => new DateTime(),
                 
