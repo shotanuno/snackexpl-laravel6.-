48322e97af6663725bf907746f5ab8295035a12a
shotanuno <20ca049e@rikkyo.ac.jp>
2022-11-11 18:44:42 +0900

    add:
    comment bookmarked.blade.phpのレイアウト、パディング指定

--- resources/views/comments/bookmarked.blade.php
+++ resources/views/comments/bookmarked.blade.php
@@ -11,10 +11,10 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>ブックマークした投稿一覧</h1>
+        <h1 style='padding: 10px 30px;'>ブックマークした投稿一覧</h1>
         <div class='cooments'>
             @foreach ($comments as $comment)
-                <div class='comment'>
+                <div class='comment' style='padding: 20px 70px;'>
                     <h2 class='title'>
                         <a href='/comments/{{ $comment->id }}'>{{ $comment->title }}</a>
                     </h2>
@@ -22,7 +22,7 @@
                     <p class='body'>{{ $comment->body }}</p>
                 </div>
             @endforeach
-            <div class='paginate'>
+            <div class='paginate' style='padding: 0 0 0 70px;'>
             {{ $comments->links() }}
             </div>
         </div>
